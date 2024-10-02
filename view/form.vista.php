b85a5cf68aeabb60de8e2f8f6784637cad5f6989
AnnaEscrisa <anna.escrisa@gmail.com>
2024-10-02 19:43:09 +0200

    estils varis

--- view/form.vista.php
+++ view/form.vista.php
@@ -7,18 +7,25 @@
         <h3>
             <?php echo $pageTitle ?>
         </h3>
-        <form action="" method="post">
-            <label for="" class="form-label">Títol*</label>
-            <input class="form-control" type="text" name="nouTitol" placeholder="Títol de l'article"
-                value="<?php echo $titol ?>">
-            <div class="form-text">
-                El títol no pot tenir més de 40 caràcters.
+        <form action="" method="post" class="articleForm">
+            <div>
+                <label for="" class="form-label">Títol*</label>
+                <input class="form-control" type="text" name="nouTitol" placeholder="Títol de l'article"
+                    value="<?php echo $titol ?>">
+                <div class="form-text">
+                    El títol no pot tenir més de 40 caràcters.
+                </div>
             </div>
-            <label for="" class="form-label">Contingut*</label>
-            <textarea class="form-control" name="nouCos" placeholder="Cos de l'article"><?php echo $cos ?></textarea>
-            <div class="form-text">
-                El cos no pot tenir més de 400 caràcters.
+
+            <div>
+                <label for="" class="form-label">Contingut*</label>
+                <textarea class="form-control" name="nouCos"
+                    placeholder="Cos de l'article"><?php echo $cos ?></textarea>
+                <div class="form-text">
+                    El cos no pot tenir més de 400 caràcters.
+                </div>
             </div>
+
             <button type="submit" class="btn btn-primary" <?php echo $isDelete ? "hidden" : "" ?>><?php echo $isEdit ? "Editar" : "Inserir" ?></button>
             <a href="form.php?isDelete=true"></a>
         </form>
