6e69da81c4790c20963283060dbfedfb58b3b578
AnnaEscrisa <anna.escrisa@gmail.com>
2024-10-07 15:25:35 +0200

    CHORE: login first commit

---
+++ notes.js
@@ -0,0 +1,16 @@
+//! PENDENT
+    // - escollir tema
+
+
+
+
+//* LOGIN
+    //-mateix per login i register
+
+    
+//* Index -- my articles
+    //utilitzar vista index per my articles
+    //
+
+    //index
+    //if session[user] -- editar i eliminar
---
+++ view/login.vista.php
@@ -0,0 +1,13 @@
+<!-- Anna Escribano -->
+<?php include "partials/_head.vista.php"; ?>
+
+<body>
+    <form action="" method="post">
+        <label class="form-label">Usuari</label>
+        <input name="usuari" class="form-control" type="text" aria-label="default input example">
+        <label class="form-label">Contrasenya</label>
+        <input name="password" type="password" class="form-control">
+        <button type="submit" class="btn btn-primary">Entra</button>
+    </form>
+    <p> No tens compte d'usuari? <a href="registre.php">Registra't</a> </p>
+</body>
